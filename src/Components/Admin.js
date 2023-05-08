f6a8c9fa07f1611376bb0672fb3b4d3a094a28e4
00Shalini <shalini07061997@gmail.com>
2023-05-09 01:11:03 +0530

    format code

--- src/Components/Admin.js
+++ src/Components/Admin.js
@@ -79,7 +79,13 @@ function AdminPanel() {
 
   return (
     <div>
-      <nav style={{ backgroundColor: "rgb(33, 37, 33)", height: "120px", marginBottom:'50px' }}>
+      <nav
+        style={{
+          backgroundColor: "rgb(33, 37, 33)",
+          height: "120px",
+          marginBottom: "50px",
+        }}
+      >
         <div>
           <div>
             <img
@@ -105,47 +111,110 @@ function AdminPanel() {
           </p>
         </div>
       </nav>
-      <div style={{display:'flex',width:'90%', justifyContent:'space-around', backgroundColor:'rgb(54, 6, 54)', height:'150px',margin:'auto', borderRadius:'20px', padding:'3.5%', border:' 1.5px solid rgb(28, 19, 204)'}}>
-      
-      <form onSubmit={updateApiKey}>
-        <label style={{fontFamily:'serif', letterSpacing:'1px', fontWeight:600, paddingRight:'20px', color:'white', fontSize:'20px'}}>
-          OpenWeather API key
-          <input
-            type="text"
-            value={apiKey}
-            style={{marginLeft:'10px', width:'250px', borderRadius:'10px', height:'40px',border:'none'}}
-            onChange={(event) => setApiKey(event.target.value)}
-          />
-        </label>
-        <button
-                  type="submit"
-                  class="btn"
-                 style={{border:'2px solid white', color:'white', backgroundColor:"rgb(54, 6, 54)", width:'120px'}}
-                >
-                  Update
-                </button>
-                
-      </form>
+      <div
+        style={{
+          display: "flex",
+          width: "90%",
+          justifyContent: "space-around",
+          backgroundColor: "rgb(54, 6, 54)",
+          height: "150px",
+          margin: "auto",
+          borderRadius: "20px",
+          padding: "3.5%",
+          border: " 1.5px solid rgb(28, 19, 204)",
+        }}
+      >
+        <form onSubmit={updateApiKey}>
+          <label
+            style={{
+              fontFamily: "serif",
+              letterSpacing: "1px",
+              fontWeight: 600,
+              paddingRight: "20px",
+              color: "white",
+              fontSize: "20px",
+            }}
+          >
+            OpenWeather API key
+            <input
+              type="text"
+              value={apiKey}
+              style={{
+                marginLeft: "10px",
+                width: "250px",
+                borderRadius: "10px",
+                height: "40px",
+                border: "none",
+              }}
+              onChange={(event) => setApiKey(event.target.value)}
+            />
+          </label>
+          <button
+            type="submit"
+            class="btn"
+            style={{
+              border: "2px solid white",
+              color: "white",
+              backgroundColor: "rgb(54, 6, 54)",
+              width: "120px",
+            }}
+          >
+            Update
+          </button>
+        </form>
 
-      <form onSubmit={updateFrequency}>
-        <label style={{fontFamily:'serif', letterSpacing:'1px', fontWeight:600, paddingRight:'20px', color:'white',  fontSize:'20px'}}>
-          Frequency
-          <input
-          style={{marginLeft:'10px', width:'250px', borderRadius:'10px', height:'40px',border:'none'}}
-            type="number"
-            value={frequency}
-            onChange={(event) => setFrequency(event.target.value)}
-          />
-        </label>
-        <button type="submit"  style={{border:'2px solid white', color:'white', backgroundColor:"rgb(54, 6, 54)", width:'120px'}} class="btn">
-          Update
-        </button>
-      </form>
+        <form onSubmit={updateFrequency}>
+          <label
+            style={{
+              fontFamily: "serif",
+              letterSpacing: "1px",
+              fontWeight: 600,
+              paddingRight: "20px",
+              color: "white",
+              fontSize: "20px",
+            }}
+          >
+            Frequency
+            <input
+              style={{
+                marginLeft: "10px",
+                width: "250px",
+                borderRadius: "10px",
+                height: "40px",
+                border: "none",
+              }}
+              type="number"
+              value={frequency}
+              onChange={(event) => setFrequency(event.target.value)}
+            />
+          </label>
+          <button
+            type="submit"
+            style={{
+              border: "2px solid white",
+              color: "white",
+              backgroundColor: "rgb(54, 6, 54)",
+              width: "120px",
+            }}
+            class="btn"
+          >
+            Update
+          </button>
+        </form>
       </div>
-      <table class="table table-striped" style={{width:"40%", margin:'auto', marginTop:'50px', border:'2px solid black', textAlign:'center'}}>
+      <table
+        class="table table-striped"
+        style={{
+          width: "40%",
+          margin: "auto",
+          marginTop: "50px",
+          border: "2px solid black",
+          textAlign: "center",
+        }}
+      >
         <thead>
           <tr>
-            <th scope="col" >Name</th>
+            <th scope="col">Name</th>
             <th scope="col">City</th>
             <th scope="col">Country</th>
             <th scope="col">Actions</th>
@@ -159,17 +228,47 @@ function AdminPanel() {
               <td>{user.country}</td>
               <td>
                 {!user.blocked && (
-                  <button onClick={() => blockUser(user._id)} class="btn" style={{border:'2px solid white', color:'white', backgroundColor:"rgb(54, 6, 54)", width:'120px'}}>Block</button>
+                  <button
+                    onClick={() => blockUser(user._id)}
+                    class="btn"
+                    style={{
+                      border: "2px solid white",
+                      color: "white",
+                      backgroundColor: "rgb(54, 6, 54)",
+                      width: "120px",
+                    }}
+                  >
+                    Block
+                  </button>
                 )}
                 {user.blocked && (
-                  <button onClick={() => unblockUser(user._id)} class="btn" style={{border:'2px solid white', color:'white', backgroundColor:"rgb(54, 6, 54)", width:'120px'}}>Unblock</button>
+                  <button
+                    onClick={() => unblockUser(user._id)}
+                    class="btn"
+                    style={{
+                      border: "2px solid white",
+                      color: "white",
+                      backgroundColor: "rgb(54, 6, 54)",
+                      width: "120px",
+                    }}
+                  >
+                    Unblock
+                  </button>
                 )}
-                <button onClick={() => deleteUser(user._id)} class="btn" style={{border:'2px solid white', color:'white', backgroundColor:"rgb(54, 6, 54)", width:'120px'}}>Delete</button>
+                <button
+                  onClick={() => deleteUser(user._id)}
+                  class="btn"
+                  style={{
+                    border: "2px solid white",
+                    color: "white",
+                    backgroundColor: "rgb(54, 6, 54)",
+                    width: "120px",
+                  }}
+                >
+                  Delete
+                </button>
               </td>
-              <td>
-               
-              
-              </td>
+              <td></td>
             </tr>
           ))}
         </tbody>
